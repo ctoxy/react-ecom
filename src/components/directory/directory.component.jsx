f200c87f1d49c10b67216a4a51dc84c709f12db2
ctoxy <cedric.bouve@gmail.com>
2020-01-10 10:53:22 +0100

    mise en place routing homepage vers hats ok

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,7 +12,7 @@ class Directory extends React.Component {
                   title: 'hats',
                   imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                   id: 1,
-                  linkUrl: 'shop/hats'
+                  linkUrl: 'hats'
                 },
                 {
                   title: 'jackets',
@@ -47,11 +47,20 @@ class Directory extends React.Component {
         return(
             <div className="directory-menu">
                 {
-                    this.state.sections.map(({title,imageUrl,id,size}) => (
-                     <MenuItem key={id} title={title} imageUrl= {imageUrl} size={size}/>   
+                    this.state.sections.map(({id, ...otherSectionProps}) => (
+                     <MenuItem key={id} {...otherSectionProps}/>   
                     ))
                 }
             </div>
+            /* equivaut a ce qui est en spreadoperatoirs otherSectionProps
+            <div className="directory-menu">
+                {
+                    this.state.sections.map(({title, imageUrl, id, size, linkUrl}) => (
+                     <MenuItem key={id} title={title} imageUrl= {imageUrl} size={size} linkUrl={linkUrl}/>   
+                    ))
+                }
+            </div>
+            */
         )
     }
 }
