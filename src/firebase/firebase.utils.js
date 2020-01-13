8a1689a2df8b3bcb8d367dd4eb8368db673502f9
ctoxy <cedric.bouve@gmail.com>
2020-01-13 10:30:32 +0100

    creation base user in firestore

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,40 @@ const config = {
     appId: "1:242385192855:web:0c3bebb46014f0750126d2",
     measurementId: "G-XSGDLGWLRD"
   }
+
+export  const createUserProfileDocument = async(userAuth, additonalData) => {
+  if (!userAuth) {
+    return;  
+  }
+  const userRef =firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  console.log(snapShot);
+  
+  if (!snapShot.exists) {
+    const {displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additonalData
+      })
+      
+    } catch (error) {
+      console.log('error creating user', error.message);
+      
+    }
+
+    return userRef;
+    
+  }
+  
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
